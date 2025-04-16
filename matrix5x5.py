905ed3119c51065df4a5b9111a7fa91fa0158598
Muhamad Ayip Priyatna <muhamadayip2021@gmail.com>
2025-04-16 16:05:15 +0700

    Add files via upload

---
+++ matrix5x5.py
@@ -0,0 +1,29 @@
+
+A = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14],
+    [16, 17, 18, 19],   
+    [21, 22, 23, 24],
+]
+
+B = [
+    [2, 3, 4, 5, 6],
+    [15, 16, 17, 18, 19],
+    [100, 101, 102, 103, 104],
+    [25, 26, 27, 28, 29],
+    [29, 30, 31, 32, 33],
+]
+# Penjumlahan Matriks A dan B
+def add_matrices(A, B):
+    result = []
+    for i in range(len(A)):
+        row = []
+        for j in range(len(A[i])):
+            row.append(A[i][j] + B[i][j])
+        result.append(row)
+    return result
+print("Hasil Penjumlahan Matriks A dan B:")
+result = add_matrices(A, B)
+for row in result:
+    print(row)
